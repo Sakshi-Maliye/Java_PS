3dd80a9a8f65185dfb530d32f7318ef628ca3698
Sakshi Maliye <148149029+Sakshi-Maliye@users.noreply.github.com>
2025-08-12 14:44:49 +0530

    Add files via upload

---
+++ sample5.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class sample5
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n=s.nextInt();
+        System.out.println("Enter l:");
+        int l=s.nextInt();
+        System.out.println("Enter h:");
+        int h=s.nextInt();
+        int tries=0;
+	int flag=0;
+        while(l<h)
+        {
+            int m=(l+h)/2;
+            System.out.println(m);
+            System.out.println("My guess is 1.Lower\n2.Higher\n3.You got it right!\n");
+            tries++;
+            int ch=s.nextInt();
+            switch(ch)
+            {
+                case 1:
+                    l=l+m;
+                    break;
+                case 2:
+                    h=h-m;
+                    break;
+                case 3:
+                    System.out.println("In tries="+tries);
+			flag=1;
+                   break;
+                default:
+                System.out.println("no");
+            }
+		if(flag==1)
+			break;
+        }
+        
+        
+    }
+}

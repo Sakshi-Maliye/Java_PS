425ef1301662f0f77b06183a80f19105c4bdbb6b
Sakshi Maliye <148149029+Sakshi-Maliye@users.noreply.github.com>
2025-08-12 14:11:24 +0530

    Add files via upload

--- sample1.java
+++ sample1.java
@@ -3,29 +3,20 @@ class sample1
 {
     public static void main(String[] args)
     {
-        Scanner s =new Scanner(System.in);
-        System.out.println("Enter n:");
-        int n=s.nextInt();
-        int[] a= new int[n];
-        int flag=0;
-        System.out.println("Enter elements:");
-        for(int i=0;i<n;i++)
-            a[i]=s.nextInt();
-        System.out.println("Enter target:");
-        int target=s.nextInt();
-        for(int i=0;i<n;i++)
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter height:");
+        int h=s.nextInt();
+        System.out.println("Enter x:");
+        int x=s.nextInt();
+        System.out.println("Enter y:");
+        int y=s.nextInt();
+        int c=0,p=0;
+        while(p<h)
         {
-            if(flag==1)
-                break;
-            for(int j=0;j<n;j++)
-            {
-                if(a[i]+a[j]==target)
-                {
-                    System.out.println(i+" "+j);
-                    flag=1;
-                    break;
-                }
-            }
+            p=p+x;
+            c++;
+            p=p-y;
         }
+        System.out.println("days="+c);
     }
 }
